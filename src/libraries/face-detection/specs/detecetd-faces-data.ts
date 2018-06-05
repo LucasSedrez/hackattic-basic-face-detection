6cb535c438ca2e8c8b41d8a1250cb4858c730793
Lucas Sedrez <lucas.sedrez@sofit4.com.br>
2018-06-05 20:58:10 -0300

    test(position.service): adding the position.service test

---
+++ src/libraries/face-detection/specs/detecetd-faces-data.ts
@@ -0,0 +1,56 @@
+export const emptyData = {};
+
+export const dataWithoutBoundingBoxes = {
+	FaceDetails: [
+		{
+			"AgeRange": {
+				"Low": 26,
+				"High": 43
+			}
+		},
+		{
+			"AgeRange": {
+				"Low": 27,
+				"High": 48
+			}
+		},
+	]
+};
+
+export const data = {
+
+	FaceDetails: [
+		{
+			"BoundingBox": {
+				"Width": 0.09444444626569748,
+				"Height": 0.09444444626569748,
+				"Left": 0.39666667580604553,
+				"Top": 0.12444444745779037
+			}
+		},
+		{
+			"BoundingBox": {
+				"Width": 0.08888889104127884,
+				"Height": 0.08888889104127884,
+				"Left": 0.023333333432674408,
+				"Top": 0.12555555999279022
+			}
+		},
+		{
+			"BoundingBox": {
+				"Width": 0.0855555534362793,
+				"Height": 0.0855555534362793,
+				"Left": 0.6411111354827881,
+				"Top": 0.6388888955116272
+			}
+		},
+		{
+			"BoundingBox": {
+				"Width": 0.0833333358168602,
+				"Height": 0.0833333358168602,
+				"Left": 0.5155555605888367,
+				"Top": 0.7666666507720947
+			}
+		}
+	]
+};
