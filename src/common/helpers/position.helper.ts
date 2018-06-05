b7ddea39824ff4ba00bc41f94cd65673e2d6057a
Lucas Sedrez <lucas.sedrez@sofit4.com.br>
2018-06-04 21:06:18 -0300

    refactor(position.helper): refactoring the helper

--- src/common/helpers/position.helper.ts
+++ src/common/helpers/position.helper.ts
@@ -1,41 +1,11 @@
-export function convertToPixels(ratios: any, pixelSize: number) {
-
-    return ratios.map(ratio => {
-
-        ratio.top *= pixelSize;
-        ratio.left *= pixelSize;
-
-        return ratio;
-    });
-}
-
 export function findCoordinates(value: number) {
 
-    if (value <= 100) {
+	value = Math.floor(value / 100);
 
-        return 0;
-    } else if (value <= 200) {
+	if (value % 100) {
 
-        return 1;
-    } else if (value <= 300) {
+		value - 1;
+	}
 
-        return 2;
-    } else if (value <= 400) {
-
-        return 3;
-    } else if (value <= 500) {
-
-        return 4;
-    } else if (value <= 600) {
-
-        return 5;
-    } else if (value <= 700) {
-
-        return 6;
-    } else if (value <= 800) {
-
-        return 7;
-    }
-
-    return value;
+	return value;
 }
