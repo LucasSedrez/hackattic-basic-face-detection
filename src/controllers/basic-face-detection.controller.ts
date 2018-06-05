119268f44e00186f1a2bf6204b1267e7a4fd0b82
Lucas Sedrez <lucas.sedrez@sofit4.com.br>
2018-06-05 11:22:09 -0300

    refactor(basic-face-detection): changing the controller to accept the facade

--- src/controllers/basic-face-detection.controller.ts
+++ src/controllers/basic-face-detection.controller.ts
@@ -4,10 +4,9 @@ import { HackatticService } from '../libraries/hackattic/hackattic.service';
 import { ACCESS_TOKEN } from '../common/settings/hackattic.credentials';
 import { S3Service } from '../libraries/aws/s3.service';
 import { S3_ACCESS_KEY, S3_SECRET_ACCESS_KEY, REKOGNITION_ACCESS_KEY, REKOGNITION_SECRET_ACCESS_KEY } from '../common/settings/aws.credentials';
-import { BUCKET_NAME, IMAGE_PATH } from '../common/settings/general';
 import { RekognitionService } from '../libraries/aws/rekognition.service';
-import { FaceDetectionService } from '../libraries/face-detection/face-detection.service';
-import { FaceDetectionImage } from '../libraries/face-detection/face-detection-image.service';
+import { PositionsService, ImageService } from '../libraries/face-detection';
+import { BasicFacade } from '../libraries/face-detection/basic';
 
 const router: Router = Router();
 
@@ -18,28 +17,42 @@ router.get('/solve', async (_req: Request, res: Response, next) => {
 		const hackattic = new HackatticService(ACCESS_TOKEN, true);
 		const s3 = new S3Service(S3_ACCESS_KEY, S3_SECRET_ACCESS_KEY);
 		const rekognition = new RekognitionService(REKOGNITION_ACCESS_KEY, REKOGNITION_SECRET_ACCESS_KEY);
-		const faceDetectonService = new FaceDetectionService();
-		const faceDetectionImage = new FaceDetectionImage(faceDetectonService, s3);
+		const positionsService = new PositionsService();
+		const imageService = new ImageService(positionsService, s3);
 
-		const imageUrl = await hackattic.getImageUrl();
+		const facade = new BasicFacade(s3, rekognition, hackattic, positionsService, imageService);
 
-		await s3.uploadImageFromUrl(imageUrl, BUCKET_NAME, IMAGE_PATH);
+		const solve = await facade.solveTheProblem();
 
-		const detectedFaces = await rekognition.detectFaces(BUCKET_NAME, IMAGE_PATH);
-
-		const positions = faceDetectonService.findPositions(detectedFaces);
-
-		const solve = await hackattic.solve(positions);
-		console.warn(solve);
-
-		const finalImage = await faceDetectionImage.generateImage(detectedFaces);
-
-		res.contentType('image/jpeg');
-		return res.send(finalImage.Body);
+		return res.send(solve);
 
 	} catch (err) {
 
 		next(err);
+	}
+
+});
+
+router.get('/faces', async (_req: Request, res: Response, next) => {
+
+	const hackattic = new HackatticService(ACCESS_TOKEN, true);
+	const s3 = new S3Service(S3_ACCESS_KEY, S3_SECRET_ACCESS_KEY);
+	const rekognition = new RekognitionService(REKOGNITION_ACCESS_KEY, REKOGNITION_SECRET_ACCESS_KEY);
+	const positionsService = new PositionsService();
+	const imageService = new ImageService(positionsService, s3);
+
+	const facade = new BasicFacade(s3, rekognition, hackattic, positionsService, imageService);
+
+	try {
+
+		const faces = await facade.showMeTheFaces();
+
+		res.contentType('image/jpeg');
+		return res.send(faces.Body);
+
+	} catch (error) {
+
+		next(error);
 	}
 
 });
