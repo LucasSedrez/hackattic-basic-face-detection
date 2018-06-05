7560d07c3c32c73a3d18ae0aca0346586181f564
Lucas Sedrez <lucas.sedrez@sofit4.com.br>
2018-06-04 21:28:18 -0300

    chore(settings): adding final image path in general settings

--- src/common/settings/general.ts
+++ src/common/settings/general.ts
@@ -1,4 +1,5 @@
 export const BUCKET_NAME = 'ls-image-recognition';
 export const IMAGE_PATH = 'media/original.jpg';
+export const FINAL_IMAGE_PATH = 'media/final.jpg';
 export const IMAGE_WIDTH = 800;
 export const IMAGE_HEIGHT = 800;
